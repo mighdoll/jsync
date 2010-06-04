0a36f7b7907a1d0bb502aa4a74dd1662d2f7e12d
Lee Mighdoll <lee@n.(none)>
2010-06-04 15:26:26 -0700

    put demoWindow code in it's own file.

---
+++ server/src/main/webapp/liquidj.com/js/demoWindow.js
@@ -0,0 +1,36 @@
+
+var LiquidDemo = LiquidDemo || {};
+
+/** Open a separate browser window to show the demo results.  
+ * (Reuses the window if it's already been opened.)  */
+var scopeWrapper = function() {
+  var demoWindow;
+
+  LiquidDemo.showDemoWindow = function(target) {
+    /** position the demo results window to the right of the main window.  LATER, test this under IE */
+    function placeDemoWindow() {
+      var x = window.screenX != undefined ? window.screenX : window.screenLeft;
+      var y = window.screenY != undefined ? window.screenY : window.screenTop;
+      var margin = 20;
+      x += window.outerWidth + margin;
+      return {x: x, y:y};
+    }
+    
+    if (!demoWindow || demoWindow.closed) {
+      var spot = placeDemoWindow();
+      demoWindow = window.open(target, "", 
+      "scrollbars=no,status=no,resizeable=yes,title-bar=no,menubar=false,toolbar=false,location=false,personalbar=false,status=false"
+       + ",height=300,width=300"
+       + ",left=" + spot.x + ",top=" + spot.y);
+      if (demoWindow == null) {
+        $log.warn("pop-up blocked");
+      }
+    }
+    
+    if (demoWindow && !demoWindow.closed) {    
+      demoWindow.focus();
+      demoWindow.location = target;
+    }
+  }
+
+}();
--- server/src/main/webapp/liquidj.com/js/main.js
+++ server/src/main/webapp/liquidj.com/js/main.js
@@ -3,46 +3,18 @@ $(function() {
   
   function subscribe() {
     registerButtons();
-//    connection.subscribe("GuestSettings", "guests", registerButtons);
+    connection.subscribe("GuestSettings", "guests", registerButtons);
   }
   function registerButtons() {
-    $('#tryIt').click(tryIt);
+    $('#tryIt').click(function() {
+      LiquidDemo.showDemoWindow("fieldExample.html");
+      $log.log("try modifyObject!");      
+      modifyObject();      
+      return false;
+    });
   }
-  
-  /** Open a browser window to show the demo results.  */
-  function showDemoWindow(target) {
-    /** position the demo results window to the right of the main window.  LATER, test this under IE */
-    function placeDemoWindow() {
-      var x = window.screenX != undefined ? window.screenX : window.screenLeft;
-      var y = window.screenY != undefined ? window.screenY : window.screenTop;
-      var margin = 20;
-      x += window.outerWidth + margin;
-      return {x: x, y:y};
-    }
-    
-    if (!demoWindow || demoWindow.closed) {
-      var spot = placeDemoWindow();
-      demoWindow = window.open(target, "", 
-      "scrollbars=no,status=no,resizeable=yes,title-bar=no,menubar=false,toolbar=false,location=false,personalbar=false,status=false"
-       + ",height=300,width=300"
-       + ",left=" + spot.x + ",top=" + spot.y);
-      if (demoWindow == null) {
-        $log.warn("pop-up blocked");
-      }
-    }
-    
-    if (demoWindow && !demoWindow.closed) {    
-      demoWindow.focus();
-      demoWindow.location = target;
-    }
-  }
-  
-  
-  function tryIt() {
-    $log.log("tryIt!");
-    showDemoWindow("fieldExample.html", "Live Object Sync")
+  function modifyObject() {
     settings.fieldExample.number_(7);
-    return false;
   }
   
   var connection = $sync.connect("http://localhost:8080/test/sync", 
