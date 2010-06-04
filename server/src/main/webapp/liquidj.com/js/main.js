fccc0299d41eb876cf40fd10f1b60e8c5156d053
Lee Mighdoll <lee@n.(none)>
2010-06-04 15:04:49 -0700

    open demo results window to the right of the main window

--- server/src/main/webapp/liquidj.com/js/main.js
+++ server/src/main/webapp/liquidj.com/js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  var settings;
+  var settings, demoWindow;
   
   function subscribe() {
     registerButtons();
@@ -8,13 +8,26 @@ $(function() {
   function registerButtons() {
     $('#tryIt').click(tryIt);
   }
-  var demoWindow;
+  
+  /** Open a browser window to show the demo results.  */
   function showDemoWindow(target) {
+    /** position the demo results window to the right of the main window.  LATER, test this under IE */
+    function placeDemoWindow() {
+      var x = window.screenX != undefined ? window.screenX : window.screenLeft;
+      var y = window.screenY != undefined ? window.screenY : window.screenTop;
+      var margin = 20;
+      x += window.outerWidth + margin;
+      return {x: x, y:y};
+    }
+    
     if (!demoWindow || demoWindow.closed) {
+      var spot = placeDemoWindow();
       demoWindow = window.open(target, "", 
-      "scrollbars=no,status=no,resizble=yes,title-bar=no,left=100,top=100,height=300,width=300,menubar=false,toolbar=false,location=false,personalbar=false,status=false");
+      "scrollbars=no,status=no,resizeable=yes,title-bar=no,menubar=false,toolbar=false,location=false,personalbar=false,status=false"
+       + ",height=300,width=300"
+       + ",left=" + spot.x + ",top=" + spot.y);
       if (demoWindow == null) {
-        console.error("pop-up blocked");
+        $log.warn("pop-up blocked");
       }
     }
     
@@ -24,8 +37,9 @@ $(function() {
     }
   }
   
+  
   function tryIt() {
-    console.log("tryIt!");
+    $log.log("tryIt!");
     showDemoWindow("fieldExample.html", "Live Object Sync")
     settings.fieldExample.number_(7);
     return false;
