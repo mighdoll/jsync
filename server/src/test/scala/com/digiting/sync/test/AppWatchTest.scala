c6d4bb72c29ce643fd12375d17d93e3e95b52941
Lee Mighdoll <lee@n.(none)>
2010-07-04 15:16:52 -0700

    created failing test for the upcoming partition notification based app watching

--- server/src/test/scala/com/digiting/sync/test/AppWatchTest.scala
+++ server/src/test/scala/com/digiting/sync/test/AppWatchTest.scala
@@ -13,14 +13,24 @@ class AppWatchTest extends Spec with ShouldMatchers with SyncFixture {
       val shared = new RamPartition("shared")
       val app1 = TempAppContext("app1")
       val named = app1.withApp {
-        shared.publish("name", TestNameObj("jerome"))
+        shared.publish("name", TestNameObj("water"))
       }
       
       val app2 = TempAppContext("app2")
-      app2.withApp {
+      val named2:TestNameObj = app2.withApp {
+        shared.published find("name") map {
+          _.asInstanceOf[TestNameObj]
+        } getOrElse fail
       }
       
-      named.name = "fred"
+      named2 should not be (named)
+      named2.name should be ("water")
+      named2
+              
+      app1.withApp {
+        named.name = "oil"
+      }
+      named2.name should be ("oil")
     }
   }
 }
