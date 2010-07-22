1e4517bc241bba42c0c3d75f18c3d81434fac617
Lee Mighdoll <lee@n.(none)>
2010-07-22 16:09:43 -0700

    added a few more tests of dataSortable.

--- server/src/main/webapp/jsync/test/testDataSortable.js
+++ server/src/main/webapp/jsync/test/testDataSortable.js
@@ -1,19 +1,56 @@
-loggedTest("dataSortable.render", function() {
-  $sync.manager.setDefaultPartition('test');
-  var seq = $sync.sequence();
-  seq.append($sync.test.nameObj({name:"albert"}));
-  seq.append($sync.test.nameObj({name:"jon"}));
-  seq.append($sync.test.nameObj({name:"christine"}));
-  var $div = 
-    $('<div/>', {
-      id:'dataSortable'
-//      'class': 'offscreen'
-      }).appendTo('body');
-  $div.dataSortable({model:seq, render:renderName});
-  ok($div.children().size() === 3);
-  ok($div.children().first().html() === 'albert');
-  ok($div.children().last().html() === 'christine');
+sortableTest('render', function(seq, $div) {
+  var $children = $div.children();
+  ok($children.size() === 3);
+  ok($children.first().html() === 'albert');
+  ok($children.last().html() === 'christine');
 });
+
+sortableTest('delete', function(seq, $div) {
+  seq.removeAt(1);
+  var $children = $div.children();
+  ok($children.size() === 2);
+});
+
+sortableTest('move', function(seq, $div) {
+  seq.moveAt(1, 2);
+  var $children = $div.children();
+  ok($children.last().html() === 'jon');
+});
+
+//sortableTest('rename', function(seq, $div) {
+//  seq.getAt(0).name_('ambika');
+//  
+//  var $children = $div.children();
+//  ok($children.first().html() === 'ambika');
+//});
+
+function sortableTest(testName, fn) {
+  loggedTest('dataSortable.' + testName, function() {
+    $sync.manager.withPartition('test', function() {
+      var seq = $sync.sequence();
+      seq.append($sync.test.nameObj({name:"albert"}));
+      seq.append($sync.test.nameObj({name:"jon"}));
+      seq.append($sync.test.nameObj({name:"christine"}));
+      
+      var $div = 
+        $('<div/>', {
+          id:'dataSortable'
+  //        'class': 'offscreen'
+          }).appendTo('body');
+      $div.dataSortable({model:seq, render:renderName});
+      fn(seq, $div);
+      // TODO cleanup $div and dataSortable..      
+//      $div.remove();
+    });  
+  });
+}
+
+function testSortable() {
+  return $sync.manager.withDefaultPartition('test', function() {
+    return seq;
+  });
+}
+
 
 function renderName(named) {
   return '<div>' + named.name + '</div>';  
